692f9c1776b7341f4234659e90849ffeffd3a6fb
rojalbati-cf <rojal.bati@cloudfactory.com>
2022-03-13 01:16:55 +0545

    Add end to end test

--- cypress/integration/todo.spec.js
+++ cypress/integration/todo.spec.js
@@ -6,8 +6,21 @@ describe('end to end test', () => {
     cy.visit('/')
   })
 
-  it('displays two todo items by default', () => {
-    expect("Yes").be.contains("Yes")
+  it('Create a Todo Item', () => {
+    cy.get('.new-todo').type('Rojal Bati{enter}')
+  })
+
+  it('Get the created Todo Item', () => {
+    cy.get('label').should('be.exist');
+    cy.get('label').should('have.text','Rojal Bati')
+  })
+
+  it('Delete the created Todo Item', () => {
+    cy.get('.destroy').click({ force: true })
+  })
+
+  it('Get the deleted Todo Item', () => {
+    cy.get('label').should('not.exist');
   })
 
 })

45608677871275f06dad87cad5a9655834ff0c26
rojalbati-cf <rojal.bati@cloudfactory.com>
2022-03-13 11:42:51 +0545

    Add semi colon

--- cypress/integration/todo.spec.js
+++ cypress/integration/todo.spec.js
@@ -7,13 +7,13 @@ describe('end to end test', () => {
   })
 
   it('Verify a Todo Item can be created', () => {
-    cy.get('.new-todo').type('Rojal Bati{enter}')
+    cy.get('.new-todo').type('Rojal Bati{enter}');
     cy.get('label').should('be.exist');
-    cy.get('label').should('have.text','Rojal Bati')
+    cy.get('label').should('have.text','Rojal Bati');
   })
 
   it('Verify a Todo Item can be Deleted', () => {
-    cy.get('.destroy').click({ force: true })
+    cy.get('.destroy').click({ force: true });
     cy.get('label').should('not.exist');
   })
 

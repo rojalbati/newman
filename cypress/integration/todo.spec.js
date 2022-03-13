5462091a58f162316f3c6bf453d8f5879abe4e8b
Rojal Bati <78461090+rojalbati-cf@users.noreply.github.com>
2022-03-13 15:14:12 +0545

    Verify Cypress dashboard comment on PR (#2)
    * Remove unwanted comment
    
    * Merge tests
    
    * Support local test without recording
    
    * Caching
    
    * Remove show node version
    
    * Modify Workflow
    
    * Add strategy
    
    * Revert
    
    * cypress workflow
    
    * Use Node removed
    
    * Add semi colon
    
    * Run test
    
    * Install false
    
    * Revert
    
    * Add Environments Newman
    
    * Update Workflow
    
    * Workflow test
    
    * Test Workflow
    
    * revert
    
    * Revert
    
    * Add Name to Checkout
    
    * remove - from uses
    
    * Add name to steps
    
    * Remove matric
    
    * Rename
    
    * Remove comment
    
    * Revert
    
    * Update file name
    
    * Upload Artificat
    
    * Artificat upload rename

--- cypress/integration/todo.spec.js
+++ cypress/integration/todo.spec.js
@@ -6,21 +6,15 @@ describe('end to end test', () => {
     cy.visit('/')
   })
 
-  it('Create a Todo Item', () => {
-    cy.get('.new-todo').type('Rojal Bati{enter}')
-  })
-
-  it('Get the created Todo Item', () => {
+  it('Verify a Todo Item can be created', () => {
+    cy.get('.new-todo').type('Rojal Bati{enter}');
     cy.get('label').should('be.exist');
-    cy.get('label').should('have.text','Rojal Bati')
+    cy.get('label').should('have.text','Rojal Bati');
   })
 
-  it('Delete the created Todo Item', () => {
-    cy.get('.destroy').click({ force: true })
+  it('Verify a Todo Item can be Deleted', () => {
+    cy.get('.destroy').click({ force: true });
+    cy.get('label').should('not.exist'); //Adding comment to run test
   })
 
-  it('Get the deleted Todo Item', () => {
-    cy.get('label').should('not.exist');
-  })
-
-}) //Comment to view the comment by Cypress Dashboard on PR 1
+})

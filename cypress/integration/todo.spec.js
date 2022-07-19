e51d09f3069d71d1322312cd796966f98fc9fbd1
Rojal Bati <54777753+rojalbati@users.noreply.github.com>
2022-07-19 13:45:51 +0545

    Minor

--- cypress/integration/todo.spec.js
+++ cypress/integration/todo.spec.js
@@ -6,13 +6,13 @@ describe('end to end test', () => {
     cy.visit('/')
   })
 
-  it('Verify a Todo Item can be created', () => {
+  it('Verify a Todo Item can be created:', () => {
     cy.get('.new-todo').type('Rojal Bati{enter}');
     cy.get('label').should('be.exist');
     cy.get('label').should('have.text','Rojal Bati');
   })
 
-  it('Verify a Todo Item can be Deleted', () => {
+  it('Verify a Todo Item can be Deleted:', () => {
     cy.get('.destroy').click({ force: true });
     cy.get('label').should('not.exist'); //Adding comment to run test
   })
